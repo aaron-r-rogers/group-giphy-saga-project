f7befc1682a9da0c83de585f204368a6b94e23fe
James Petran <jm.ptrn@gmail.com>
2022-01-27 13:17:50 -0600

    basic favorites page is started

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,9 +1,19 @@
-function Favorites () {
-    return (
-        <>
-        Favorites
-        </>
-    )
+import Container from '@mui/material/Container';
+
+
+function Favorites() {
+  // need to grab favorite gifs .state
+  // need to grab all categories .state
+
+  const GifCard = () => {
+
+  }
+
+  return (
+    <Container maxWidth="lg">
+      Favorites
+    </Container>
+  )
 }
 
 export default Favorites;
