a4ec2ef72755105794e70f5f346aaeab8a27e352
aaron-r-rogers <rogers.aaron.r@gmail.com>
2022-01-27 11:23:46 -0600

    Search and make favorites saga/redux relationships created; working fetch favorites

--- src/index.js
+++ src/index.js
@@ -17,17 +17,44 @@ sagaMiddleware.run(rootSaga);
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_IMAGE', fetchImage);
+    yield takeEvery('SEARCH_RESULTS', searchResults);//GET
+    yield takeEvery('MAKE_FAVORITE', makeFavorite);//POST
+    yield takeEvery('FETCH_FAVORITES', fetchFavorites);//GET
 }
 
-const images {
-    id: ''
-}
+// Reducer that holds our results
+const searchResults = (state = [], action) => {
+    if(action.type === 'SEARCH_GIF') {
+        console.log('search payload:', action.payload);
+        return [...state, ...action.payload]
+    }
+    return state;
+};
+
+function* makeFavorite (action) {
+    console.log('in makeFavorite', action);
+
+    // action.payload looks like this...
+    // {
+    //     image_id: '',
+    //     category_id: ''
+    // }
+
+    // Post favorite to the server
+    yield axios.post('/api/favorite', action.payload);
+
+    // Run the fetchFavorites saga to get latest favs...
+    yield put({
+        type: 'FETCH_FAVORITES'
+    });
+};
+
+
 
 // Create one store that all components can use
 const storeInstance = createStore(
     combineReducers({
-        
+        searchResults,
     }),
     // Add sagaMiddleware to our store
     applyMiddleware(sagaMiddleware, logger),
