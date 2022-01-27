f2079a98c58b5d3d1f591c95ce114c21376b591a
James Petran <jm.ptrn@gmail.com>
2022-01-27 13:39:27 -0600

    basic card shape

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,4 +1,7 @@
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import './Favorites.css';
 
 
 function Favorites() {
@@ -6,12 +9,39 @@ function Favorites() {
   // need to grab all categories .state
 
   const GifCard = () => {
+    // 
+    // 
+    // 
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image={''}
+          // gif.url here 👆 after i get redux store obj imported
+          alt={''}
+        // gif.title here 👆 after i get redux store obj imported
+        />
+        <Categories />
+      </Card>
+    )
+  }
 
+  const Categories = () => {
+    // 
+    // 
+    // list of categories w conditional rendering with selected! or not...
+    return (
+      <div className="categories-container">
+
+      </div>
+    )
   }
 
   return (
     <Container maxWidth="lg">
       Favorites
+      <GifCard />
     </Container>
   )
 }
