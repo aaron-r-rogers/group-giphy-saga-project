c6fadd980763531057e3c6bf0ad98bb94b0c1de1
Ken Dang <k3ndang@gmail.com>
2022-01-27 13:17:42 -0600

    added a form on search and dispatch

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,49 @@
+import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+
 function Search () {
+    const dispatch = useDispatch();
+    // const searchResult = useSelector((store) store.searchResult)
+    let [newGif, setNewGif] = useState('');
+
+
+    // search bar that have local state
+    // submit button
+    // gif display with favorite button
+
+
+    const searchGiphy = () => {
+        console.log('search', searchGiphy);
+
+
+        dispatch({
+            type: 'SEARCH_GIF'
+        })
+    }
+
     return (
         <>
-        Search
+        <div>
+            <h2>Search Form</h2>
+            <form>
+                <input 
+                    type='text'
+                    placeholder='Search'
+                    value={newGif}
+                    onChange={(event) => setNewGif(event.target.value)}
+                />
+                <button onClick={searchGiphy}>Search</button>
+            </form>
+
+            {/* <div>
+                {searchResult.map((gif) => (
+                    <img  key={searchResult.id}  />
+                ))}
+            </div> */}
+
+        </div>
         </>
     )
 }
