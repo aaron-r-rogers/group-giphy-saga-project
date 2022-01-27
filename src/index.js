24a31e4f1fe413efcb8b65d8f4a706f2fa9c9475
aaron-r-rogers <rogers.aaron.r@gmail.com>
2022-01-27 13:18:17 -0600

    Fetch categories saga and corresponding reducer created; working on search connection to router

--- src/index.js
+++ src/index.js
@@ -17,16 +17,40 @@ sagaMiddleware.run(rootSaga);
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('SEARCH_RESULTS', searchResults);//GET
+    // does searchResults need to be a Saga or just a reducer?
+    yield takeEvery('GET_SEARCH', getSearch);//GET
     yield takeEvery('MAKE_FAVORITE', makeFavorite);//POST
     yield takeEvery('FETCH_FAVORITES', fetchFavorites);//GET
-}
+    yield takeEvery('FETCH_CATEGORIES', fetchCategories);//GET
+};
+
+function* getSearch (action) {
+    try{
+    console.log('made it to getSearch');
+    let response = yield axios.get('/search', {params: {q: action.payload}});
+    console.log('getSearch response.data is:', response.data);
+    //send data to searchReducer
+    yield put({
+        type: 'SEARCH_GIF',
+        payload: response.data
+    });
+    }
+    catch(err) {
+        console.log('getSearch failed', err);
+        // this reducer doesn't exist yet; could
+        // put the error there to be rendered in the DOM
+        yield put({
+            type: 'SET_ERROR',
+            payload: err
+        })
+    }
+};
 
 // Reducer that holds our results
 const searchResults = (state = [], action) => {
     if(action.type === 'SEARCH_GIF') {
         console.log('search payload:', action.payload);
-        return [...state, ...action.payload]
+        return action.payload
     }
     return state;
 };
@@ -49,12 +73,74 @@ function* makeFavorite (action) {
     });
 };
 
+function* fetchFavorites () {
+    try{
+    console.log('made it to fetchFavorites');
+    let response = yield axios.get('/api/favorite');
+    console.log('index.js fetchFavorites response.data is:', response.data);
+    //send data to favoritesReducer
+    yield put({
+        type: 'SET_FAVORITES',
+        payload: response.data
+    });
+    }
+    catch(err) {
+        console.log('fetchFavorites failed', err);
+        // this reducer doesn't exist yet; could
+        // put the error there to be rendered in the DOM
+        yield put({
+            type: 'SET_ERROR',
+            payload: err
+        })
+    }
+};
 
+const favoritesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FAVORITES':
+            return [...state, ...action.payload];
+        default:
+            return state;
+    }
+};
+
+function* fetchCategories () {
+    try{
+    console.log('made it to fetchCategories');
+    let response = yield axios.get('/api/category');
+    console.log('index.js fetchCategories response.data is:', response.data);
+    //send data to categoriesReducer
+    yield put({
+        type: 'SET_CATEGORIES',
+        payload: response.data
+    });
+    }
+    catch(err) {
+        console.log('fetchCategories failed', err);
+        // this reducer doesn't exist yet; could
+        // put the error there to be rendered in the DOM
+        yield put({
+            type: 'SET_ERROR',
+            payload: err
+        })
+    }
+};
+
+// Reducer that holds our results
+const categoriesReducer = (state = [], action) => {
+    if(action.type === 'SET_CATEGORIES') {
+        console.log('categoriesReducer payload:', action.payload);
+        return action.payload
+    }
+    return state;
+};
 
 // Create one store that all components can use
 const storeInstance = createStore(
     combineReducers({
         searchResults,
+        favoritesReducer,
+        categoriesReducer
     }),
     // Add sagaMiddleware to our store
     applyMiddleware(sagaMiddleware, logger),
