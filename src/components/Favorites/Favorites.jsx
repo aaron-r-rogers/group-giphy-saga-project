b08e99a01a3b2a0652a5f578fd1d76fdf7e48bc3
James Petran <jm.ptrn@gmail.com>
2022-01-27 21:47:19 -0600

    made the gif finally display on DOM using its url saved in table

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -10,53 +10,37 @@ function Favorites() {
   // need to grab all categories .state
   const categoryList = useSelector(store => store.categoriesReducer);
   const faveList = useSelector(store => store.favoritesReducer);
-  console.log('faveList',faveList);
+  console.log('faveList', faveList);
 
-  faveList.map((fav) => (
-    console.log('fav',fav)
-  // fav.map(favorite => console.log(favorite)
-  // )
-  )
-  )
-  // each gif has a card element that displays the gif and its assigned categories
+  const removeCategory = (favId, catId) => {
+    dispatch({
+      type: 'REMOVE_CATEGORY',
+      payload: {
+        category_id: catId,
+        favorite_id: favId
+      }
+    })
+  }
 
-  //each gifCard has these categories underneath it, and they are toggle-able
-
-  //single category
-
-
-  // const removeCategory = (favId, catId) => {
-  //   dispatch({
-  //     type: 'REMOVE_CATEGORY',
-  //     payload: {
-  //       category_id: catId,
-  //       favorite_id: favId
-  //     }
-  //   })
-  // }
-
-  // const addCategory = (favId, catId) => {
-  //   dispatch({
-  //     type: 'ADD_CATEGORY',
-  //     payload: {
-  //       category_id: catId,
-  //       favorite_id: favId
-  //     }
-  //   })
-  // }
+  const addCategory = (favId, catId) => {
+    dispatch({
+      type: 'ADD_CATEGORY',
+      payload: {
+        category_id: catId,
+        favorite_id: favId
+      }
+    })
+  }
 
   return (
-    <>
-      favorite page
-    </>
-    // <Container maxWidth="lg">
-    //   {faveList.map((fav) => 
-    //     <GifCard
-    //       key={fav.id}
-    //       favorite={fav}
-    //     />
-    //   )}
-    // </Container>
+    <Container maxWidth="lg">
+      {faveList.map((favorite) =>
+        <GifCard
+          key={favorite.id}
+          favorite={favorite}
+        />
+      )}
+    </Container>
   )
 }
 
